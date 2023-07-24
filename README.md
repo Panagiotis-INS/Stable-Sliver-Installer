c1fccd722eeb768c64018107c384f5f405cccf3c
Neuro <panagiotisfisk@gmail.com>
2023-07-24 22:03:40 +0300

    Changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Stable-Sliver-Installer
 This is a small script that downloads and sets up the latest STABLE version of the sliver C2 framework.
+
+## Installation:
+
+```bash
+chmod +x ./install.sh
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+##
+sudo cp ./sliver.service /etc/systemd/system/sliver.service
+echo "[+] Service file ready"
+sudo wget https://github.com/BishopFox/sliver/releases/download/v1.5.39/sliver-client_linux -O /root/sliver-client
+sudo wget https://github.com/BishopFox/sliver/releases/download/v1.5.39/sliver-client_linux.sig -O /root/sliver-client.sig
+sudo chmod +x /root/sliver-client
+echo "[+] Downloaded client"
+sudo wget https://github.com/BishopFox/sliver/releases/download/v1.5.39/sliver-server_linux -O /root/sliver-server
+sudo wget https://github.com/BishopFox/sliver/releases/download/v1.5.39/sliver-server_linux.sig -O /root/sliver-server.sig
+sudo chmod +x /root/sliver-server
+echo "[+] Downloaded server"
+sudo cp /root/sliver-client /usr/bin/sliver
+sudo chmod +x /usr/bin/sliver
+sudo systemctl enable sliver.service
+sudo systemctl start sliver.service
+echo "[+] Everything reay"
